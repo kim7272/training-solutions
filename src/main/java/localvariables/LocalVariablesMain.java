c5924f3dcf4657bb957acc72e249c2e93838c464
balaz <balazs_h@hotmail.com>
2020-11-07 08:05:05 +0100

    Helyi változók feladat

---
+++ src/main/java/localvariables/LocalVariablesMain.java
@@ -0,0 +1,27 @@
+package localvariables;
+
+public class LocalVariablesMain {
+
+    public static void main(String[] args) {
+
+        boolean b;
+        //System.out.println(b);
+        b = true;
+
+        int a = 2;
+
+        int i = 2, j = 3;
+
+        int k = i;
+
+      //  System.out.println(l);
+
+        String s;
+        s = "Hello World";
+        String t = s;
+
+        { int x = 0;
+        System.out.println(a);}
+      //  System.out.println(x);
+    }
+}
