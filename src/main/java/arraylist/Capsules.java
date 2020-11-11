5be6f044aa161db60f96ed7225b18367ca5d1042
balaz <balazs_h@hotmail.com>
2020-11-11 15:40:07 +0100

    Capsules osztály feladat

--- src/main/java/arraylist/Capsules.java
+++ src/main/java/arraylist/Capsules.java
@@ -1,5 +1,4 @@
 package arraylist;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -22,21 +21,25 @@ public void removeLast() {
   public  List<String> getColors(){
     return colors;
     }
-
     public static void main(String[] args) {
     Capsules capsules = new Capsules();
+
     capsules.addLast("blue");
     capsules.addLast("white");
     capsules.addLast("green");
     capsules.addLast("black");
     capsules.addLast("red");
+
     System.out.println(capsules.getColors());
+
     capsules.addFirst("yellow");
     System.out.println(capsules.getColors());
     capsules.removeFirst();
+
     System.out.println(capsules.getColors());
     capsules.removeLast();
+
     System.out.println(capsules.getColors());
-    System.out.println(capsules.getColors());
+
     }
 }
