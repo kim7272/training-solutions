f7170198e9399911b1d500cdd05ce4ed23c408cb
balaz <balazs_h@hotmail.com>
2020-11-25 17:59:48 +0100

    week05d03 - ListCounterTest

---
+++ src/test/java/week05d03/ListCounterTest.java
@@ -0,0 +1,48 @@
+package week05d03;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class ListCounterTest {
+
+    @Test
+    public void countListTest(){
+        //Given
+        List<String> listToCheck = Arrays.asList("alma", "ata", "laca", "mama", "arte");
+
+        //When
+        int count = new ListCounter().countList(listToCheck);
+
+        //Then
+        assertEquals(3, count);
+
+    }
+
+    @Test
+    public void countListTest2() {
+        //Given
+        List<String> listToCheck = Arrays.asList("Alma", "ata", "laca", "mama", "Arte");
+
+        //When
+        int count = new ListCounter().countList(listToCheck);
+
+        //Then
+        assertEquals(3, count);
+    }
+
+    @Test
+    public void countListTest3() {
+        //Given
+        List<String> listToCheck = Arrays.asList("");
+
+        //When
+        int count = new ListCounter().countList(listToCheck);
+
+        //Then
+        assertEquals(0, count);
+    }
+}
