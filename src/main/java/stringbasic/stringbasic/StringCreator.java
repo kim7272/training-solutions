de0670952cd8141c3d5b700544028eaac272399e
balaz <balazs_h@hotmail.com>
2020-11-28 20:05:34 +0100

    stringbasic - stringbasic

---
+++ src/main/java/stringbasic/stringbasic/StringCreator.java
@@ -0,0 +1,17 @@
+package stringbasic.stringbasic;
+
+public class StringCreator {
+
+
+
+
+    public String createStringForHeap(){
+        String string = new String();
+        return string;
+    }
+
+    public String createStringForPool(){
+        String string = "";
+        return string;
+    }
+}
