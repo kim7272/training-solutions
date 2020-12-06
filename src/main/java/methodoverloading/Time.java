03387949e5035ebe6a1b5c36405050e750b75bc7
balaz <balazs_h@hotmail.com>
2020-12-06 10:15:03 +0100

    Methodoverloading - Time

---
+++ src/main/java/methodoverloading/Time.java
@@ -0,0 +1,68 @@
+package methodoverloading;
+
+import javax.swing.*;
+
+public class Time {
+
+    private int hours;
+    private int minutes;
+    private int seconds;
+
+    public Time( int hours){
+        this.hours = hours;
+    }
+    public Time(int hours, int minutes){
+        this.hours = hours;
+        this.minutes = minutes;
+    }
+    public Time(int hours, int minutes, int seconds) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+    public Time(Time time) {
+            this.hours = time.getHours();
+            this.minutes = time.getMinutes();
+            this.seconds = time.getSeconds();
+    }
+
+    public int getHours(){
+        return hours;
+    }
+    public int getMinutes(){
+        return minutes;
+    }
+    public int getSeconds(){
+        return seconds;
+    }
+
+    public boolean isEqual(Time time){
+        if (this.hours == time.getHours() && this.minutes == time.getMinutes() && this.seconds == time.getSeconds()){
+            return true;}
+        else{
+            return false;}
+    }
+    public boolean isEqual(int hours, int minutes, int seconds){
+        if (this.hours == hours && this.minutes == minutes && this.seconds == seconds){
+            return true;}
+        else{
+            return false;}
+    }
+    public boolean isEarlier(Time time){
+        if ((this.hours < time.getHours()) || ((this.hours == time.getHours()) && (this.minutes < time.getMinutes())) ||
+           (((this.hours == time.getHours()) && (this.minutes == time.getMinutes()))&& (this.seconds < time.getSeconds()))){
+            return true;}
+        else{
+            return false;}
+    }
+    public boolean isEarlier(int hours, int minutes, int seconds){
+        if (this.hours < hours || (this.hours == hours && this.minutes < minutes) ||
+          ((this.hours == hours && this.minutes == minutes)&& this.seconds < seconds)){
+            return true;}
+        else{
+            return false;}
+    }
+
+
+
+}
