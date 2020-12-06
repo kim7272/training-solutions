d4a032b0a4f05c32a3f5ef895390c2b6f03fc2be
balaz <balazs_h@hotmail.com>
2020-12-06 10:17:18 +0100

    Methodchain - Robot

---
+++ src/main/java/methodchain/robot/Robot.java
@@ -0,0 +1,23 @@
+package methodchain.robot;
+
+public class Robot {
+
+    private int distance;
+    private int azimut;
+
+    public int getDistance(){
+        return distance;
+    }
+    public int getAzimut(){
+        return azimut;
+    }
+    public Robot go(int meter){
+        distance = distance + meter;
+        return this;
+    }
+
+    public Robot rotate(int angle){
+        azimut = azimut + angle;
+        return this;
+    }
+}
