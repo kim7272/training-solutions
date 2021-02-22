d6ea48c1a45df5e91455bbf3a2c52f03d0cce6c8
balaz <balazs_h@hotmail.com>
2021-02-22 20:29:23 +0100

    exam03 retake01 CdvCheck

--- src/main/java/exam03retake01/CdvCheck.java
+++ src/main/java/exam03retake01/CdvCheck.java
@@ -1,6 +1,38 @@
 package exam03retake01;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CdvCheck {
 
+    private List<Integer> numbers = new ArrayList<>();
+    private int number;
+    private int sum;
+    private int rest;
+    private boolean result;
 
+
+    public boolean check(String tax) {
+
+        if (tax.length() != 10) {
+            throw new IllegalArgumentException("Wrong parameter!");
+        }
+        for (int i = 0; i < tax.length(); i++) {
+            number = Integer.parseInt(tax.substring(i, i + 1));
+            numbers.add(number);
+        }
+        for (int i = 1; i < numbers.size() - 1; i++) {
+            sum = sum + number * i;
+        }
+        rest = sum % 11;
+        {
+            if (rest == numbers.get(9)) {
+                result = true;
+            } else {
+                result = false;
+            }
+
+        }
+        return result;
+    }
 }
